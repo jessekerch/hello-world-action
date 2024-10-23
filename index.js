f1e58507068ea7e179ee7bf4c5dac1e2fb98b99c
jessekerch <jesse.kercheval@gmail.com>
2024-10-22 21:39:51 -0700

    Import module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from "@actions/core";
+import github from "@actions/github";
 
 try {
   // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  
+
   // Record the time of greeting as an output
-  const time = (new Date()).toTimeString();
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
 } catch (error) {
   // Handle errors and indicate failure
